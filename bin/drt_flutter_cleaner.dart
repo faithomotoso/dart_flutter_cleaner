fc444c92be74d66d2b2bfa8d4cd6450d8066952a
Faith Omotoso <faithomotoso@gmail.com>
2023-10-07 14:41:01 +0100

    init

---
+++ bin/drt_flutter_cleaner.dart
@@ -0,0 +1,5 @@
+import 'package:drt_flutter_cleaner/drt_flutter_cleaner.dart' as drt_flutter_cleaner;
+
+void main(List<String> arguments) {
+  print('Hello world: ${drt_flutter_cleaner.calculate()}!');
+}
---
+++ test/drt_flutter_cleaner_test.dart
@@ -0,0 +1,8 @@
+import 'package:drt_flutter_cleaner/drt_flutter_cleaner.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
